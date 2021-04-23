f0ff3049587cbac57aa80f066af4f26f88649f69
Jack Huntington-Rainey <jack@Jacks-MacBook-Pro-2.local>
2021-04-23 14:13:07 -0400

    Create url patterns

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('addwish/', views.WishCreate.as_view(), name='add_wish'),
+  path('delete/<int:wish_id>/', views.wish_todo, name='wish_todo'),
 ]
