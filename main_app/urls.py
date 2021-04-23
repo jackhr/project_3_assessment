b06dac7790cd19b14fc572c0b757926a95cb464f
Jack Huntington-Rainey <jack@Jacks-MacBook-Pro-2.local>
2021-04-23 14:40:20 -0400

    Implement full CRD

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('addwish/', views.WishCreate.as_view(), name='add_wish'),
-  path('delete/<int:wish_id>/', views.delete_wish, name='wish_todo'),
+  path('delete/<int:wish_id>/', views.delete_wish, name='delete_wish'),
 ]
